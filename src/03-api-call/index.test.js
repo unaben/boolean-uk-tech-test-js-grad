b4387b02bc1e5e39ff51de8b942c0804e7103c58
Stuart Gray <stuart.gray@reachplc.com>
2021-04-30 14:20:36 +0100

    new tests

--- src/03-api-call/index.test.js
+++
@@ -1,9 +0,0 @@
-const fetchLukeSkywalker = require('.');
-
-describe('fetchLukeSkywalker', () => {
-  it('returns correct information about Luke Skywalker', async () => {
-    const actualResult = await fetchLukeSkywalker();
-
-    expect(actualResult).toMatchSnapshot();
-  });
-});
